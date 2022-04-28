bfae8a1706dcf50fa30930940fdc43630c3d543d
tyler <tbird22@stu.aberdeenhall.com>
2022-04-28 13:35:41 -0700

    :)

---
+++ app/src/main/java/com/example/cookieclicker/Quack.java
@@ -0,0 +1,11 @@
+package com.example.cookieclicker;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Quack extends LinkedList implements Deque {
+
+    public Quack(){
+
+    }
+}
